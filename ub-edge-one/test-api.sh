6f00b684866cdd3e52227ecc440975710b33d672
dennis.borrmann <dennis.borrmann@eecc.de>
2025-09-12 11:47:29 +0200

    working

--- ub-edge-one/test-api.sh
+++
@@ -1,7 +0,0 @@
-export KEY=TEST2; export BASE="https://dataprovider-x-controlplane.construct-x.ub-edge-one.de/management"
- 
-curl -sS -X POST "$BASE/v3/assets/request" -H "X-Api-Key: $KEY"
- 
-curl -sS -X POST "$BASE/v3/policydefinitions/request" -H "X-Api-Key: $KEY"
- 
-curl -sS -X POST "$BASE/v3/contractdefinitions/request" -H "X-Api-Key: $KEY"
